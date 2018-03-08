09f9fb263eb8d92dc733ab7fb15452ab0dd208ec
taji <tajitsu@naoj.org>
2018-03-08 13:28:16 -1000

    Prepare for Gtk+3

--- main.c
+++ main.c
@@ -16154,7 +16154,6 @@ void popup_message(gchar* stock_id, gint delay, ...){
   va_start(args, delay);
 
   dialog = gtk_dialog_new();
-  gtk_window_set_decorated(GTK_WINDOW(dialog),FALSE);
 
   gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_MOUSE);
   gtk_container_set_border_width(GTK_CONTAINER(dialog),5);
