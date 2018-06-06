fcb7ba202bd7d43543ed9b76d6b5f5fcd97d66c7
taji <tajitsu@naoj.org>
2018-06-05 17:38:53 -1000

    Revised time calc in plot to the JD based one

--- main.h
+++ main.h
@@ -2175,7 +2175,7 @@ gdouble date_to_jd();
 void calc_moon();
 double get_julian_day_of_equinox();
 gboolean draw_plot_cairo();
-//void get_plot_time_meridian();
+void get_plot_time_meridian(typHOE*, gfloat);
 gdouble deg_sep();
 void ext_play();
 
