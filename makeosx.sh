2e2ff9e3c953c2de27b913355baeb88e66207be2
Akito Tajitsu <tajitsu@naoj.org>
2019-11-05 13:27:02 -1000

    Revision for Non-Sidereal targets

--- makeosx.sh
+++ makeosx.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 touch configure configure.ac aclocal.m4 Makefile.am Makefile.in
 
-./configure --with-gtkmac CPPFLAGS=-I/usr/local/opt/openssl/include \
+## libffi & openssl installed as keg-only in Homebrew
+./configure --with-gtkmac \
+	    --with-localedir="Contents/Resources/share/locale" \
+	    --with-themedir="Contents/Resources/share/icons" \
+	    PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig" \
   && make
